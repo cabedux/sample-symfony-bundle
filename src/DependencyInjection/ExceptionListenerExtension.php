93b51acf5a0c7a73519eeddf5cafa8124d6e8adf
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2023-02-15 23:04:34 +0100

    move config

--- src/DependencyInjection/ExceptionListenerExtension.php
+++ src/DependencyInjection/ExceptionListenerExtension.php
@@ -11,7 +11,7 @@ class ExceptionListenerExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator('@FmoLoggerBundle/config/services.xml'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.yaml');
     }
 }
