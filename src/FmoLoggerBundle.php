d65e45aebfba2a5fcb6672509c0391f7f48df373
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2023-02-16 22:17:08 +0100

    small changes

--- src/FmoLoggerBundle.php
+++ src/FmoLoggerBundle.php
@@ -2,10 +2,21 @@
 
 namespace Fmo\FmoLoggerBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class FmoLoggerBundle extends Bundle
+class FmoLoggerBundle extends AbstractBundle
 {
+    public function loadExtension(
+        array $config,
+        ContainerConfigurator $container,
+        ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+
+    }
+
     public function getPath(): string
     {
         return \dirname(__DIR__);
